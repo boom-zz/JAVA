d499d389bac1a5c350c2d4d8bcecff6b5c952a32
boom <58553781+boom-zz@users.noreply.github.com>
2019-12-08 10:47:50 +0800

    Sum 1!+3！+...+10!

--- text3.java
+++ text3.java
@@ -3,7 +3,7 @@ public class text3
 	public static void main(String [] argu) {
 		long result = 0;
 		long f = 1;
-		for (int i = 1; i < 10; i++){
+		for (int i = 1; i <= 10; i++){
 			f = f * i;
 			result += f;
 		}
