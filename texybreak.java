b56275f1cd5b785f08b1765defcd8f151d0861c2
boom <58553781+boom-zz@users.noreply.github.com>
2019-12-08 13:02:58 +0800

    break

---
+++ texybreak.java
@@ -0,0 +1,14 @@
+class  texybreak
+{
+	public static void main(String[] args) 
+	{
+		int stop = 4;
+		for (int i=0; i<4; i++) {
+			if (i==stop)
+			{
+			break;
+			}
+			System.out.println("i=" + i);
+		}
+	}
+}
