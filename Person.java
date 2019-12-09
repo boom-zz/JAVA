d45bd21e9ac8bcc2a0a0d32769a05ed5d8db05a2
boom <58553781+boom-zz@users.noreply.github.com>
2019-12-09 16:46:15 +0800

    Person

---
+++ Person.java
@@ -0,0 +1,20 @@
+class Person 
+{
+	int id;
+	int age;
+	Person(int _id, int _age)
+	{
+		id = _id;
+		age = _age;
+	}
+	public static void main(String[] args) 
+	{
+		Person tom = new Person(1,9);
+		point john = new point();
+	}
+}
+class point
+{
+	point()
+	{}
+}
