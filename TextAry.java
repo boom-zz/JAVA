ddfca4f1e7ed0a48a14eeaae3ab2093d076bd522
boom <58553781+boom-zz@users.noreply.github.com>
2019-12-12 14:51:36 +0800

    array

---
+++ TextAry.java
@@ -0,0 +1,23 @@
+class TextAry
+{
+	public static void main(String[] args) 
+	{
+		int s [];
+		s = new int[5];
+		for (int i=0; i<5 ; i++ )
+		{
+			s[i] = 2*i + 1;
+			System.out.println(s[i]);
+	    }
+		int[] a = {2,3,4,5,7};
+		for (int i=0; i<a.length ; i++ )
+		{
+			System.out.println(a[i]);
+		}
+		for (int i=0; i<args.length ;i++ )
+		{
+			System.out.println(args[i]);
+		}
+	}
+		
+}
